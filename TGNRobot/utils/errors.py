c2489377b7ff19e7e7128b9aa6fd189dca5ccf02
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-11-23 18:00:36 +0300

    Update errors.py

--- TGNRobot/utils/errors.py
+++ TGNRobot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 from TGNRobot import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001196083551)
+LOG_GROUP_ID = int(-1001722071521
 
 def split_limits(text):
     if len(text) < 2048:
